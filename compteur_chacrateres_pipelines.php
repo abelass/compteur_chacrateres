a3252fcdb2d9de834ba1cfda5deceff8348e105a
abelass <websolutions@mychacra.net>
2012-12-06 17:50:01 +0100

    début du compteur fonctionnel, faudrait maintenant le faire configurable depuis config

--- compteur_chacrateres_pipelines.php
+++ compteur_chacrateres_pipelines.php
@@ -8,12 +8,38 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 
-/*
- * Un fichier de pipelines permet de regrouper
- * les fonctions de branchement de votre plugin
- * sur des pipelines existants.
- */
+function compteur_chacrateres_header_prive($flux){
+    $flux .= '<script type="text/javascript" charset="utf-8" src="'.find_in_path('javascript/jquery.simplyCountable.js').'"></script>';
+    $flux .= "<script type='text/javascript' charset='utf-8'>
+        $(document).ready(function()
+        {
+          $('#descriptif').simplyCountable({
+               counter: '.compteur_descriptif',
+                countDirection: 'up',
+                maxCount: 100,
+          });
+          $('#text_area').simplyCountable({
+            counter: '.compteur_text_area',
+            countType: 'characters',
+            countDirection: 'up',
+            maxCount: 500,
+          });
+        });
+    </script>";
+    $flux .= '<link id="cssprivee" href="'.find_in_path('css/compteur_chacrateres.css').'" type="text/css" rel="stylesheet">';
+    return $flux;
+}
 
-
-
+function compteur_chacrateres_recuperer_fond($flux){
+    //Insertion des onglets de langue
+    $texte=$flux['data']['texte'];
+    if ($flux['args']['fond'] == 'formulaires/editer_article'){
+        $descriptif='<div class="compteur"><span class="compteur_descriptif"></span>'._T('compteur_caractres:caracteres').'</div>';
+        $textarea='<div class="compteur"><span class="compteur_text_area"></span>'._T('compteur_caractres:caracteres').'</div>';        
+        $patterns = array('/<textarea name=\'descriptif\'/','/<textarea name=\'texte\' /');
+        $replacements = array($descriptif.'<textarea name=\'descriptif\'',$textarea.'<textarea name=\'texte\'');                        
+       $flux['data']['texte'] = preg_replace($patterns,$replacements,$texte,1);
+    }
+    return $flux;
+}
 ?>
